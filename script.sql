67fd8f7fd245d96053edd055a2a3c0118d3b78e5
Aliceluis <128178161+Aliceluis@users.noreply.github.com>
2024-04-04 10:07:00 -0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -5,5 +5,5 @@ CREATE TABLE Agendamentos (
     id INT PRIMARY KEY IDENTITY,
     data_agendamento VARCHAR(10) NOT NULL,
     hora VARCHAR(10) NOT NULL,
-    motivo_reserva VARCHAR(50) NOT NULL
+    reserva VARCHAR(50) NOT NULL
 );
