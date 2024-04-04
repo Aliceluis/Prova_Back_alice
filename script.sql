2bf926f2862c57bd9e1da9dc1daded653893835f
Aliceluis <128178161+Aliceluis@users.noreply.github.com>
2024-04-04 10:07:32 -0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -4,6 +4,6 @@ use senai
 CREATE TABLE Agendamentos (
     id INT PRIMARY KEY IDENTITY,
     data_agendamento VARCHAR(10) NOT NULL,
-    hora VARCHAR(10) NOT NULL,
+    horario VARCHAR(10) NOT NULL,
     reserva VARCHAR(50) NOT NULL
 );
